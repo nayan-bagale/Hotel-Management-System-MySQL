dae39e71f2a8f9cae66e142d5b53aeacbb4f5a3d
“Nayan <nvbagale@yahoo.com>
2022-10-31 02:07:32 +0530

    new

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.use(express.static('static'))
+
+app.get('/',(req,res) => {
+    res.send('Nice!!!')
+});
+
+app.listen(port, () => console.log('Connected on localhost:4000'))
